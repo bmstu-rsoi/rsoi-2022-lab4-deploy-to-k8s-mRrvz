af0f20766c5d909c28023a66fb01c7def799d484
Alexey Romanov <romanov.alexey2000@gmail.com>
2022-11-05 14:06:01 +0300

    src: introduce services base
    (cherry picked from commit 3b400ce5ae82115631800ff4872d6e31ae770736)
    Signed-off-by: Alexey Romanov <romanov.alexey2000@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bmstu-rsoi/rsoi-2022-lab2-microservices-mRrvz
+
+go 1.17
---
+++ src/gateway-service/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func main() {
+	port := 1234
+	log.Println("server is listening on port: ", port)
+}
